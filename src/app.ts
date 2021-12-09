faf0f4c16b3900efc0ff114fb35452bc8e9b5048
Tom Nagle <tomanagle@gmail.com>
2021-12-10 08:18:59 +1000

    feat: reply to messages

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,7 @@
 require("dotenv").config();
-import Discord, { Intents } from "discord.js";
-import { google } from "googleapis";
-import fs from "fs";
-import path from "path";
-import { Tokens } from "./types/app";
-
-const tokens: Tokens = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "../tokens.json"), {
-    encoding: "utf8",
-  })
-);
-
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_OAUTH_CLIENT_ID,
-  process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-  "http://localhost:3000"
-);
-
-oauth2Client.setCredentials(tokens);
-
-const yt = google.youtube({ version: "v3", auth: oauth2Client });
+import { get } from "lodash";
+import Discord, { Intents, MessageEvent } from "discord.js";
+import { getVideoStats, ytUrlParser } from "./lib/youtube";
 
 const client = new Discord.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -31,34 +13,19 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 });
 
-async function getVideoStats({ videoId }: { videoId: string }) {
-  const { data } = await yt.videos.list({
-    part: ["snippet", "statistics"],
-    id: [videoId],
-  });
+client.on<string>("messageCreate", async function (message) {
+  const embeddedUrl = get(message, "embeds[0].url");
 
-  if (!data.items) return `Could not find a video with the id ${videoId}`;
+  const videoId = embeddedUrl ? ytUrlParser(embeddedUrl) : null;
 
-  const video = data?.items[0];
+  if (videoId) {
+    const res = videoId
+      ? await getVideoStats({ videoId, isReplyToMessage: true })
+      : "Please provide a video ID";
 
-  if (!video) {
-    return `Could not find video with ID ${videoId}. Copy the ID from the video URL.`;
+    message.reply(res);
   }
-
-  const likes = parseInt(video.statistics?.likeCount || "0", 10);
-
-  const dislikes = parseInt(video.statistics?.dislikeCount || "0", 10);
-
-  const total = likes + dislikes;
-
-  const ratio = Math.round((likes / total) * 100 * 10) / 10;
-
-  const views = parseInt(video.statistics?.viewCount || "0", 10).toLocaleString(
-    "en-us"
-  );
-
-  return `${video.snippet?.title}\n\n👍 Likes: ${likes}\n\n👎 Dislikes: ${dislikes}\n\n📈 Ratio: ${ratio}%\n\n👀 Views: ${views}\n\n💬 Comments: ${video.statistics?.commentCount}\n https://youtube.com/watch?v=${videoId}`;
-}
+});
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) {
---
+++ src/lib/youtube.ts
@@ -0,0 +1,83 @@
+import { google, youtube_v3 } from "googleapis";
+import fs from "fs";
+import path from "path";
+import { Tokens } from "../types/app";
+
+const tokens: Tokens = JSON.parse(
+  fs.readFileSync(path.resolve(process.cwd() + "/tokens.json"), {
+    encoding: "utf8",
+  })
+);
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_OAUTH_CLIENT_ID,
+  process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+  "http://localhost:3000"
+);
+
+oauth2Client.setCredentials(tokens);
+
+const yt = google.youtube({ version: "v3", auth: oauth2Client });
+
+function formatVideoData(video: youtube_v3.Schema$Video) {
+  const likes = parseInt(video.statistics?.likeCount || "0", 10);
+
+  const dislikes = parseInt(video.statistics?.dislikeCount || "0", 10);
+
+  const total = likes + dislikes;
+
+  const ratio = Math.round((likes / total) * 100 * 10) / 10;
+
+  const views = parseInt(video.statistics?.viewCount || "0", 10).toLocaleString(
+    "en-us"
+  );
+
+  const title = video.snippet?.title;
+
+  const commentCount = video.statistics?.commentCount;
+
+  return { likes, dislikes, ratio, views, title, commentCount };
+}
+
+export async function getVideoStats({
+  videoId,
+  isReplyToMessage = false,
+}: {
+  videoId: string;
+  isReplyToMessage?: boolean;
+}) {
+  const { data } = await yt.videos.list({
+    part: ["snippet", "statistics"],
+    id: [videoId],
+  });
+
+  if (!data.items) return `Could not find a video with the id ${videoId}`;
+
+  const video = data?.items[0];
+
+  if (!video) {
+    return `Could not find video with ID ${videoId}. Copy the ID from the video URL.`;
+  }
+
+  const { views, likes, dislikes, commentCount, ratio, title } =
+    formatVideoData(video);
+
+  if (isReplyToMessage) {
+    return `Beep bop, here's some stats about that video \n👍 Likes: ${likes}\n\n👎 Dislikes: ${dislikes}\n\n📈 Ratio: ${ratio}%\n\n👀 Views: ${views}\n\n💬 Comments: ${commentCount}`;
+  }
+
+  return `${title}\n\n👍 Likes: ${likes}\n\n👎 Dislikes: ${dislikes}\n\n📈 Ratio: ${ratio}%\n\n👀 Views: ${views}\n\n💬 Comments: ${commentCount}\n https://youtube.com/watch?v=${videoId}`;
+}
+
+const ytVideoRegExp =
+  /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+
+export function ytUrlParser(url: string): string | null {
+  const match = url.match(ytVideoRegExp);
+
+  if (match && match[7].length == 11) {
+    return match[7];
+  }
+
+  return null;
+}
