2b36eb19e093c12c4ddc46b3eeb67b109ecb6916
sidwebworks <xodedeveloper@gmail.com>
2021-12-11 20:31:30 +0530

    👌 IMPROVE: Message formatting function

--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -6,15 +6,15 @@ import { Tokens } from "../types/app";
 import { Message } from "discord.js";
 
 const tokens: Tokens = JSON.parse(
-  fs.readFileSync(path.resolve(process.cwd() + "/tokens.json"), {
-    encoding: "utf8",
-  })
+	fs.readFileSync(path.resolve(process.cwd() + "/tokens.json"), {
+		encoding: "utf8",
+	})
 );
 
 const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_OAUTH_CLIENT_ID,
-  process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-  "http://localhost:3000"
+	process.env.GOOGLE_OAUTH_CLIENT_ID,
+	process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+	"http://localhost:3000"
 );
 
 oauth2Client.setCredentials(tokens);
@@ -22,103 +22,119 @@ oauth2Client.setCredentials(tokens);
 const yt = google.youtube({ version: "v3", auth: oauth2Client });
 
 function formatVideoData(video: youtube_v3.Schema$Video) {
-  const likes = parseInt(video.statistics?.likeCount || "0", 10);
+	const likes = parseInt(video.statistics?.likeCount || "0", 10);
 
-  const dislikes = parseInt(video.statistics?.dislikeCount || "0", 10);
+	const dislikes = parseInt(video.statistics?.dislikeCount || "0", 10);
 
-  const total = likes + dislikes;
+	const total = likes + dislikes;
 
-  const ratio = Math.round((likes / total) * 100 * 10) / 10;
+	const ratio = Math.round((likes / total) * 100 * 10) / 10;
 
-  const views = parseInt(video.statistics?.viewCount || "0", 10).toLocaleString(
-    "en-us"
-  );
+	const views = parseInt(video.statistics?.viewCount || "0", 10).toLocaleString("en-us");
 
-  const title = video.snippet?.title;
+	const title = video.snippet?.title;
 
-  const commentCount = video.statistics?.commentCount;
+	const commentCount = video.statistics?.commentCount;
 
-  return { likes, dislikes, ratio, views, title, commentCount };
+	let result = [
+		{ name: "title", msg: `🔮 Title: ${title}` },
+		{ name: "likes", msg: `👍 Likes: ${likes}` },
+		{ name: "dislikes", msg: `👎 Dislikes: ${dislikes}` },
+		{ name: "ratio", msg: `📈 Ratio: ${ratio}` },
+		{ name: "views", msg: `👀 Views: ${views}` },
+		{ name: "commentCount", msg: `💬 Comments: ${commentCount}` },
+		{ name: "link", msg: `https://youtube.com/watch?v=${video.id}` },
+	];
+
+	if (isNaN(ratio)) {
+		/**
+		 * These fields can be disabled by the video author,
+		 * if they are, change the field msg.
+		 */
+		const disabled = ["likes", "dislikes", "ratio"];
+
+		result = result.map((el) =>
+			disabled.includes(el.name)
+				? { ...el, msg: `${el.msg.split(": ")[0]}: Disabled by the video creator` }
+				: el
+		);
+	}
+
+	return result;
 }
 
 export async function getVideoStats({
-  videoId,
-  isReplyToMessage = false,
+	videoId,
+	isReplyToMessage = false,
 }: {
-  videoId: string;
-  isReplyToMessage?: boolean;
+	videoId: string;
+	isReplyToMessage?: boolean;
 }) {
-  const { data } = await yt.videos.list({
-    part: ["snippet", "statistics"],
-    id: [videoId],
-  });
+	const { data } = await yt.videos.list({
+		part: ["snippet", "statistics"],
+		id: [videoId],
+	});
+  
+	if (!data.items) return `Could not find a video with the id ${videoId}`;
 
-  if (!data.items) return `Could not find a video with the id ${videoId}`;
+	const video = data?.items[0];
 
-  const video = data?.items[0];
+	if (!video) {
+		return `Could not find video with ID ${videoId}. Copy the ID from the video URL.`;
+	}
 
-  if (!video) {
-    return `Could not find video with ID ${videoId}. Copy the ID from the video URL.`;
-  }
+	const formatted = formatVideoData(video);
 
-  const { views, likes, dislikes, commentCount, ratio, title } =
-    formatVideoData(video);
-
-  // TODO format the messages with an array, that way we can take an array of properties to exclude
-  if (isReplyToMessage) {
-    return `${title}\n\n👍 Likes: ${likes}\n\n👎 Dislikes: ${dislikes}\n\n📈 Ratio: ${ratio}%\n\n👀 Views: ${views}\n\n💬 Comments: ${commentCount}`;
-  }
-
-  return `${title}\n\n👍 Likes: ${likes}\n\n👎 Dislikes: ${dislikes}\n\n📈 Ratio: ${ratio}%\n\n👀 Views: ${views}\n\n💬 Comments: ${commentCount}\n https://youtube.com/watch?v=${videoId}`;
+	return generateMessage(formatted, isReplyToMessage);
 }
 
 const ytVideoRegExp =
-  /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+	/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+
+const generateMessage = (data: ReturnType<typeof formatVideoData>, isReply: boolean) => {
+	const stats = isReply ? data.filter((el) => el.name !== "link") : data;
+	const message = stats.map((el) => el.msg).join(`\n\n`);
+
+	return message;
+};
 
 export function getVideoIdFromYtUrl(url: string): string | null {
-  try {
-    const u = new URL(url);
+	try {
+		const u = new URL(url);
 
-    const searchParams = new URLSearchParams(u.searchParams);
+		const searchParams = new URLSearchParams(u.searchParams);
 
-    if (searchParams.has("v")) {
-      return searchParams.get("v");
-    }
-  } catch (e) {}
+		if (searchParams.has("v")) {
+			return searchParams.get("v");
+		}
+	} catch (e) {}
 
-  const match = url.match(ytVideoRegExp);
+	const match = url.match(ytVideoRegExp);
 
-  if (match && match[7].length == 11) {
-    return match[7];
-  }
+	if (match && match[7].length == 11) {
+		return match[7];
+	}
 
-  return null;
+	return null;
 }
 
-export function getVideoIdFromMessage({
-  message,
-}: {
-  message: Message;
-}): string | null {
-  const embeddedUrl = get(message, "embeds[0].url");
+export function getVideoIdFromMessage({ message }: { message: Message }): string | null {
+	const embeddedUrl = get(message, "embeds[0].url");
 
-  if (embeddedUrl) {
-    return getVideoIdFromYtUrl(embeddedUrl);
-  }
+	if (embeddedUrl) {
+		return getVideoIdFromYtUrl(embeddedUrl);
+	}
 
-  const { content } = message;
+	const { content } = message;
 
-  if (
-    content.startsWith("https://youtu.be/") ||
-    content.startsWith("https://www.youtu")
-  ) {
-    return getVideoIdFromYtUrl(message.content);
-  }
+	if (content.startsWith("https://youtu.be/") || content.startsWith("https://www.youtu")) {
+		return getVideoIdFromYtUrl(message.content);
+	}
 
-  // Try avoid using the regex
-  if (ytVideoRegExp.test(content)) {
-    return getVideoIdFromYtUrl(message.content);
-  }
+	// Try avoid using the regex
+	if (ytVideoRegExp.test(content)) {
+		return getVideoIdFromYtUrl(message.content);
+	}
 
-  return null;
+	return null;
 }
