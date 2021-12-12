010d0c267d0f356fae4283152cacbe6f15f0ff95
Tom Nagle <tomanagle@gmail.com>
2021-12-13 08:15:18 +1000

    fix: formatting

--- src/lib/__test__/youtube.test.ts
+++ src/lib/__test__/youtube.test.ts
@@ -1,41 +1,41 @@
 import { getVideoIdFromMessage, getVideoStats } from "../youtube";
 
 describe("youtube", () => {
-	const videoId = "a_N3ixrB2rY";
+  const videoId = "a_N3ixrB2rY";
 
-	it("should get the videoID from full urls", () => {
-		expect(
-			getVideoIdFromMessage({
-				// @ts-ignore
-				message: {
-					content: `https://www.youtube.com/watch?v=${videoId}&ab_channel=TomDoesTech`,
-				},
-			})
-		).toBe(videoId);
-	});
+  it("should get the videoID from full urls", () => {
+    expect(
+      getVideoIdFromMessage({
+        // @ts-ignore
+        message: {
+          content: `https://www.youtube.com/watch?v=${videoId}&ab_channel=TomDoesTech`,
+        },
+      })
+    ).toBe(videoId);
+  });
 
-	it("should get the videoID from short urls", () => {
-		expect(
-			getVideoIdFromMessage({
-				// @ts-ignore
-				message: {
-					content: `https://youtu.be/${videoId}`,
-				},
-			})
-		).toBe(videoId);
-	});
+  it("should get the videoID from short urls", () => {
+    expect(
+      getVideoIdFromMessage({
+        // @ts-ignore
+        message: {
+          content: `https://youtu.be/${videoId}`,
+        },
+      })
+    ).toBe(videoId);
+  });
 });
 
 describe("Format message", () => {
-	const videoId = "NGxVLnJKhP8";
+  const videoId = "NGxVLnJKhP8";
 
-	it("should format with disabled error if likes are disabled", async () => {
-		const video = await getVideoStats({ videoId, isReplyToMessage: false });
-		expect(video.includes("Disabled by the video creator")).toBe(true);
-	});
+  it("should format with disabled error if likes are disabled", async () => {
+    const video = await getVideoStats({ videoId, isReplyToMessage: false });
+    expect(video.includes("Disabled by the video creator")).toBe(true);
+  });
 
   it("should not show video link on reply messages", async () => {
-		const video = await getVideoStats({ videoId, isReplyToMessage: true });
-		expect(video.includes("https://youtu.be/")).toBe(false);
-	});
+    const video = await getVideoStats({ videoId, isReplyToMessage: true });
+    expect(video.includes("https://youtu.be/")).toBe(false);
+  });
 });
--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -6,15 +6,15 @@ import { Tokens } from "../types/app";
 import { Message } from "discord.js";
 
 const tokens: Tokens = JSON.parse(
-	fs.readFileSync(path.resolve(process.cwd() + "/tokens.json"), {
-		encoding: "utf8",
-	})
+  fs.readFileSync(path.resolve(process.cwd() + "/tokens.json"), {
+    encoding: "utf8",
+  })
 );
 
 const oauth2Client = new google.auth.OAuth2(
-	process.env.GOOGLE_OAUTH_CLIENT_ID,
-	process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-	"http://localhost:3000"
+  process.env.GOOGLE_OAUTH_CLIENT_ID,
+  process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+  "http://localhost:3000"
 );
 
 oauth2Client.setCredentials(tokens);
@@ -22,119 +22,134 @@ oauth2Client.setCredentials(tokens);
 const yt = google.youtube({ version: "v3", auth: oauth2Client });
 
 function formatVideoData(video: youtube_v3.Schema$Video) {
-	const likes = parseInt(video.statistics?.likeCount || "0", 10);
+  const likes = parseInt(video.statistics?.likeCount || "0", 10);
 
-	const dislikes = parseInt(video.statistics?.dislikeCount || "0", 10);
+  const dislikes = parseInt(video.statistics?.dislikeCount || "0", 10);
 
-	const total = likes + dislikes;
+  const total = likes + dislikes;
 
-	const ratio = Math.round((likes / total) * 100 * 10) / 10;
+  const ratio = Math.round((likes / total) * 100 * 10) / 10;
 
-	const views = parseInt(video.statistics?.viewCount || "0", 10).toLocaleString("en-us");
+  const views = parseInt(video.statistics?.viewCount || "0", 10).toLocaleString(
+    "en-us"
+  );
 
-	const title = video.snippet?.title;
+  const title = video.snippet?.title;
 
-	const commentCount = video.statistics?.commentCount;
+  const commentCount = video.statistics?.commentCount;
 
-	let result = [
-		{ name: "title", msg: `🔮 Title: ${title}` },
-		{ name: "likes", msg: `👍 Likes: ${likes}` },
-		{ name: "dislikes", msg: `👎 Dislikes: ${dislikes}` },
-		{ name: "ratio", msg: `📈 Ratio: ${ratio}` },
-		{ name: "views", msg: `👀 Views: ${views}` },
-		{ name: "commentCount", msg: `💬 Comments: ${commentCount}` },
-		{ name: "link", msg: `https://youtube.com/watch?v=${video.id}` },
-	];
+  let result = [
+    { name: "title", msg: `🔮 Title: ${title}` },
+    { name: "likes", msg: `👍 Likes: ${likes}` },
+    { name: "dislikes", msg: `👎 Dislikes: ${dislikes}` },
+    { name: "ratio", msg: `📈 Ratio: ${ratio}` },
+    { name: "views", msg: `👀 Views: ${views}` },
+    { name: "commentCount", msg: `💬 Comments: ${commentCount}` },
+    { name: "link", msg: `https://youtube.com/watch?v=${video.id}` },
+  ];
 
-	if (isNaN(ratio)) {
-		/**
-		 * These fields can be disabled by the video author,
-		 * if they are, change the field msg.
-		 */
-		const disabled = ["likes", "dislikes", "ratio"];
+  if (isNaN(ratio)) {
+    /**
+     * These fields can be disabled by the video author,
+     * if they are, change the field msg.
+     */
+    const disabled = ["likes", "dislikes", "ratio"];
 
-		result = result.map((el) =>
-			disabled.includes(el.name)
-				? { ...el, msg: `${el.msg.split(": ")[0]}: Disabled by the video creator` }
-				: el
-		);
-	}
+    result = result.map((el) =>
+      disabled.includes(el.name)
+        ? {
+            ...el,
+            msg: `${el.msg.split(": ")[0]}: Disabled by the video creator`,
+          }
+        : el
+    );
+  }
 
-	return result;
+  return result;
 }
 
 export async function getVideoStats({
-	videoId,
-	isReplyToMessage = false,
+  videoId,
+  isReplyToMessage = false,
 }: {
-	videoId: string;
-	isReplyToMessage?: boolean;
+  videoId: string;
+  isReplyToMessage?: boolean;
 }) {
-	const { data } = await yt.videos.list({
-		part: ["snippet", "statistics"],
-		id: [videoId],
-	});
-  
-	if (!data.items) return `Could not find a video with the id ${videoId}`;
+  const { data } = await yt.videos.list({
+    part: ["snippet", "statistics"],
+    id: [videoId],
+  });
 
-	const video = data?.items[0];
+  if (!data.items) return `Could not find a video with the id ${videoId}`;
 
-	if (!video) {
-		return `Could not find video with ID ${videoId}. Copy the ID from the video URL.`;
-	}
+  const video = data?.items[0];
 
-	const formatted = formatVideoData(video);
+  if (!video) {
+    return `Could not find video with ID ${videoId}. Copy the ID from the video URL.`;
+  }
 
-	return generateMessage(formatted, isReplyToMessage);
+  const formatted = formatVideoData(video);
+
+  return generateMessage(formatted, isReplyToMessage);
 }
 
 const ytVideoRegExp =
-	/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
 
-const generateMessage = (data: ReturnType<typeof formatVideoData>, isReply: boolean) => {
-	const stats = isReply ? data.filter((el) => el.name !== "link") : data;
-	const message = stats.map((el) => el.msg).join(`\n\n`);
+const generateMessage = (
+  data: ReturnType<typeof formatVideoData>,
+  isReply: boolean
+) => {
+  const stats = isReply ? data.filter((el) => el.name !== "link") : data;
+  const message = stats.map((el) => el.msg).join(`\n\n`);
 
-	return message;
+  return message;
 };
 
 export function getVideoIdFromYtUrl(url: string): string | null {
-	try {
-		const u = new URL(url);
+  try {
+    const u = new URL(url);
 
-		const searchParams = new URLSearchParams(u.searchParams);
+    const searchParams = new URLSearchParams(u.searchParams);
 
-		if (searchParams.has("v")) {
-			return searchParams.get("v");
-		}
-	} catch (e) {}
+    if (searchParams.has("v")) {
+      return searchParams.get("v");
+    }
+  } catch (e) {}
 
-	const match = url.match(ytVideoRegExp);
+  const match = url.match(ytVideoRegExp);
 
-	if (match && match[7].length == 11) {
-		return match[7];
-	}
+  if (match && match[7].length == 11) {
+    return match[7];
+  }
 
-	return null;
+  return null;
 }
 
-export function getVideoIdFromMessage({ message }: { message: Message }): string | null {
-	const embeddedUrl = get(message, "embeds[0].url");
+export function getVideoIdFromMessage({
+  message,
+}: {
+  message: Message;
+}): string | null {
+  const embeddedUrl = get(message, "embeds[0].url");
 
-	if (embeddedUrl) {
-		return getVideoIdFromYtUrl(embeddedUrl);
-	}
+  if (embeddedUrl) {
+    return getVideoIdFromYtUrl(embeddedUrl);
+  }
 
-	const { content } = message;
+  const { content } = message;
 
-	if (content.startsWith("https://youtu.be/") || content.startsWith("https://www.youtu")) {
-		return getVideoIdFromYtUrl(message.content);
-	}
+  if (
+    content.startsWith("https://youtu.be/") ||
+    content.startsWith("https://www.youtu")
+  ) {
+    return getVideoIdFromYtUrl(message.content);
+  }
 
-	// Try avoid using the regex
-	if (ytVideoRegExp.test(content)) {
-		return getVideoIdFromYtUrl(message.content);
-	}
+  // Try avoid using the regex
+  if (ytVideoRegExp.test(content)) {
+    return getVideoIdFromYtUrl(message.content);
+  }
 
-	return null;
+  return null;
 }
